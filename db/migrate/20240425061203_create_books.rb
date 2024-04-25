f821d3276447be2a35678ccff6b1a27b68936634
Yasuo Honda <yahonda@pingcap.com>
2024-04-25 15:19:55 +0900

    Add `t.belongs_to :author, foreign_key: true`

--- db/migrate/20240425061203_create_books.rb
+++ db/migrate/20240425061203_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[7.1]
   def change
     create_table :books do |t|
       t.string :title
-
+      t.belongs_to :author, foreign_key: true
       t.timestamps
     end
   end
